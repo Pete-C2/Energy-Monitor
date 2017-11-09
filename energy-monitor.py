ca75f4b3915ea8a1cb2206cd8d31a0192fd428c1
Pete-C2 <github@crighton.me.uk>
2017-11-09 18:41:24 +0000

    Added current monitor

--- energy-monitor.py
+++ energy-monitor.py
@@ -1,19 +1,36 @@
 #!/usr/bin/python
+"""Energy Monitor
+Battery energy monitor/discharger
+"""
  
 import spidev
 import time
+import datetime
 
-#Define Variables
-delay = 0.5
+# Define Variables
+delay = 1.0
+
+# Voltage Monitoring
 voltage_channel = 0
-Rgnd = 180 #ohms
-Rfeed = 1000 #ohms
+RgndV = 180 #ohms - Resistor from channel 0 to ground
+RfeedV = 1000 #ohms - Resistor from channel 0 to positive battery terminal
 Vref = 3.3 #volts
 
-#Create SPI
+# Current Monitoring
+current_channel = 1
+RgndI = 3900 #ohms - Resistor from channel 1 to ground
+RfeedI = 1800 #ohms - Resistor from channel 1 to ACS712 Vout
+sensitivity = 185 #mV/A ACS712x05B
+zero_current = 2.5 #V
+
+# Battery Discharge
+minimum_battery_voltage = 10.8 #V - Lead Acid 12V battery
+energy = 0 #Ah
+
+# Create SPI
 spi = spidev.SpiDev()
 spi.open(0, 0)
- 
+
 def readadc(adcnum):
     # read SPI data from the MCP3008, 8 channels in total
     if adcnum > 7 or adcnum < 0:
@@ -22,14 +39,35 @@ def readadc(adcnum):
     data = ((r[1] & 3) << 8) + r[2]
     return data
     
- 
+   
 while True:
+    # Measure the voltage
     voltage_value = readadc(voltage_channel)
     measured_voltage = voltage_value * Vref / 1024
-    voltage = measured_voltage * (Rgnd + Rfeed) / Rgnd
+    voltage = measured_voltage * (RgndV + RfeedV) / RgndV
+
+    # Measure the current
+    current_value = readadc(current_channel)
+    current_voltage = current_value * Vref / 1024
+    ACS = current_voltage * (RgndI + RfeedI) / RgndI
+    current_offset = ACS - zero_current
+    current = current_offset * 1000 / sensitivity
+
+    # Calculate the cumulative energy
+    energy = energy + (current * delay / 3600)
     
     print "---------------------------------------"
     print("Voltage Value: %d" % voltage_value)
     print("Measured voltage: %f" % measured_voltage)
     print("Voltage %f" % voltage)
+    print "--"
+    print("Current Value: %d" % current_value)
+    print("Measured voltage: %f" % current_voltage)
+    print("ACS Voltage: %f" % ACS)
+    print("Current offset: %f" % current_offset)
+    print("Current A: %f" % current)
+    print "--"
+    print("Energy Ah: %f" % energy)
+    
+    
     time.sleep(delay)
